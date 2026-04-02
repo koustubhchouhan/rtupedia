da2255222013a8e6c78bb197a26f0fbcd04d9d95
kp <kanchanprajapat676@gmail.com>
2026-04-02 23:31:34 +0530

    PYQ finally correct linked

--- src/pages/BranchContent.js
+++ src/pages/BranchContent.js
@@ -234,7 +234,7 @@ const BranchContent = () => {
                 {grp.subjectName} ({grp.subjectCode})
               </div>
               {grp.pyqs.map((q, idx) => (
-                <a key={idx} className="pyq-paper-link" href={`${window.location.origin + q.pdf}`}  target="_blank" rel="noopener noreferrer">
+                <a key={idx} className="pyq-paper-link" href={`${q.pdf}`}  target="_blank" rel="noopener noreferrer">
                   📄 {q.title}
                 </a>
               ))}
